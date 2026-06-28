caa1a59a3ef5d6124d8b0044b09c3e6899cc3a81
atiq-glitch <atiqjaved36@gmail.com>
2026-06-29 04:13:23 +0500

    add initial files

---
+++ index.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+template <classname t>
+class t {
+    T a, T b;
+    public:
+    T add(a,b){
+        return a+b;
+    }
+};
+int main(){
+    t a;
+    a.add(10,15);
+}
